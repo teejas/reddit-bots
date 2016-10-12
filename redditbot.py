91b6ca706f8856733872dbbbe616370f993b3b90
Tejas Siripurapu <tejassiripurapu@Mac-Daddy.local>
2016-10-11 23:10:06 -0500

    finished bot

---
+++ redditbot.py
@@ -0,0 +1,17 @@
+import praw
+
+user_agent = ("Karma Breakdown v1.0 by /u/teejas"
+                "https://github.com/teejas/reddit-bots/")
+                
+r = praw.Reddit(user_agent = user_agent)
+user_name = "_Daimon_"
+user = r.get_redditor(user_name)
+gen = user.get_submitted()
+karma_by_subreddit = {}
+for thing in gen:
+    subreddit = thing.subreddit.display_name
+    karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0) + thing.score)
+
+import pprint
+
+pprint.pprint(karma_by_subreddit)
